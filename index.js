759285d49f41f64e808cbb375cef20b9ed16411a
Chai <isaigomezhernandez.com>
2019-09-06 00:41:31 -0500

    No permanece en el label

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // función para pintar en el label la lista de fibinacci
 const pintar = () => {
   const numero = document.getElementById("numero").value;
-  document.getElementById("resul").innerHTML = fibonaci(numero);
+  document.getElementById("resul").innerHTML = fibonacci(numero);
 };
 // se ejecuta cuando se pulsa el boton cal
 document.getElementById("cal").addEventListener("click", pintar);
 
-// funcion que devuelve una lista con la secuencia de fibonacci
-const fibonaci = n => {
+// función que devuelve una lista con la secuencia de fibonacci
+const fibonacci = n => {
   let v1 = 0,
     v2 = 1,
     s;
@@ -26,4 +26,3 @@ const fibonaci = n => {
     return a;
   }
 };
-//console.log(fibonaci(5));
